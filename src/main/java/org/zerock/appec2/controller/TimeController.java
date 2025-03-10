be4df308f83aa90e2efc4a836109c75924748d8e
lehoyeon <ghdus4043@gmail.com>
2025-03-10 16:25:38 +0900

    테스트 베포1

---
+++ src/main/java/org/zerock/appec2/controller/TimeController.java
@@ -0,0 +1,36 @@
+package org.zerock.appec2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/time")
+public class TimeController {
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/now")
+    public Map<String, String> getNow(){
+
+        String now = "";
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+            ResultSet resultSet = preparedStatement.executeQuery();
+        ){
+            resultSet.next();
+            now = resultSet.getString(1);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Map.of("now", now);
+    }
+}
